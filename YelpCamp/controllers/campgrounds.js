aa356b73ef8aa4de696ae332f0658d18585f07df
acsnengr <aaroncyril.nuisa.acsn@gmail.com>
2025-11-26 19:38:21 +0800

    added functionality for deleting images

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -53,6 +53,7 @@ module.exports.renderEditForm = async (req, res) => {
 //edit controller
 module.exports.editCamp = async (req, res) => {
   const { id } = req.params;
+  console.log(req.body);
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
@@ -62,6 +63,15 @@ module.exports.editCamp = async (req, res) => {
   }));
   campground.images.push(...imgs);
   await campground.save();
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+    console.log(campground);
+  }
   req.flash("success", "Successfully updated campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
